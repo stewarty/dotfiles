fa67c5283c9e80ec118145589821b0d26675aa5d
Stewart Ritchie <stewart@Stewarts-MBP.lan>
2019-12-30 08:42:54 +0000

    inital test of dotfiles

---
+++ all.sh
@@ -0,0 +1,15 @@
+if [ -f $HOME/dotfiles/functions-git.sh ]; then
+    . $HOME/dotfiles/functions-git.sh
+fi
+
+if [ -f $HOME/dotfiles/defaults.sh ]; then
+    . $HOME/dotfiles/defaults.sh
+fi
+
+if [ -f $HOME/dotfiles/ui.sh ]; then
+    . $HOME/dotfiles/ui.sh
+fi
+
+if [ -f $HOME/dotfiles/paths.sh ]; then
+    . $HOME/dotfiles/paths.sh
+fi
---
+++ defaults.sh
@@ -0,0 +1,8 @@
+#   Set Default Editor (change 'Nano' to the editor of your choice)
+#   ------------------------------------------------------------
+    export EDITOR=/usr/bin/nano
+
+#   Set default blocksize for ls, df, du
+#   from this: http://hints.macworld.com/comment.php?mode=view&cid=24491
+#   ------------------------------------------------------------
+    export BLOCKSIZE=1k
